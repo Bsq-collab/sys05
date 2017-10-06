17506491f89b7ba4077177796ebac3d15c96b616
Rihui Zheng <superpatato@Rihuis-MacBook-Pro.local>
2017-10-05 20:47:34 -0400

    modified main

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main(){
     printf("Testing mystrlen:\n");
     char mystry[50] = "DW is the best CS teacher";
     printf("My string length: %d\n",mystrlen(mystry));
-    printf("real string length: %lu\n ", strlen(mystry));
+    printf("real string length: %lu\n", strlen(mystry));
    
-    printf("Testing mystrcat: \n");
+    printf("\n\nTesting mystrcat: \n");
     char mystrcat1[200]="The best math teacher";
     char mystrcat2[200]= "Mr.Kats is ";
     char strcat1[200]="The best math teacher";
@@ -16,28 +16,28 @@ int main(){
     printf("source: %s\n", mystrcat1);
     printf("dest: %s\n", mystrcat2);
     
-    printf("concatonation: %s\n", strcat(strcat2,strcat1));
+    printf("concatonation (built in/Expected): %s\n", strcat(strcat2,strcat1));
     printf("MY concatonation: %s\n", mystrcat(mystrcat2,mystrcat1));
 
-    printf("testing mystrncpy:\n\n");
-    char mystrcpy1[200]="Rihui";
-    char mystrcpy2[200]= "Sucks ";
-    char strcpy1[200]="Rihui";
-    char strcpy2[200]="Sucks ";
+    printf("\n\ntesting mystrncpy:\n");
+    char mystrcpy1[200]="Its Monday! Noo!";
+    char mystrcpy2[200]= "Its Friday! Yay!";
+    char strcpy1[200]="Its Monday! Noo!";
+    char strcpy2[200]="Its Friday! Yay!";
     printf("source: %s\n", mystrcpy1);
     printf("dest: %s\n", mystrcpy2);
     
-    printf("ncopy: %s\n", strncpy(strcpy2,strcpy1,2));
-    printf("MY ncopy: %s\n", mystrncpy(mystrcpy2,mystrcpy1,2));
+    printf("ncopy (built in/Expected): %s\n", strncpy(strcpy2,strcpy1,7));
+    printf("MY ncopy: %s\n", mystrncpy(mystrcpy2,mystrcpy1,7));
 
-      printf("testing mystrcmp:\n\n");
+      printf("\n\ntesting mystrcmp:\n");
      char s1[200]= "let's search this string";
      char s2[200]= "abcdefghik";
          
      printf("strcmp (built in/Expected): %d\n", strcmp(s1,s2));
      printf("MY strcmp: %d\n", mystrcmp(s1,s2));
 
-     printf("testing mystrchr:\n\n");
+     printf("\n\ntesting mystrchr:\n");
      char search[200]= "let's search this string";
      char item= 's';
      printf("string to search: %s\n", search);
